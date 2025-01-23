5c7d06ea1c046b40ed5f01692a7b1c2d097ccdc8
Ahmed Emad <113151367+AhmedEmad226@users.noreply.github.com>
2025-01-23 12:38:52 +0000

    Add files via upload

--- src/Components/NotFound/Notfound.jsx
+++ src/Components/NotFound/Notfound.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function Notfound() {
   return (
-    <div>Notfound</div>
+    <div className="d-flex justify-content-center align-items-center vh-100 ">
+      <h1 className='text-center text-danger'>Not Found 404    </h1>
+      <i className='fas fa-bug fa-3x'></i>
+    </div>
   )
 }
